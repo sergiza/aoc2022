402c35e3e16702690fb85625509f50ad4ff7075d
“sergiza” <sergiza1994@gmail.com>
2022-12-12 16:46:24 +0100

    day6

---
+++ src/bin/06.rs
@@ -0,0 +1,57 @@
+use std::collections::VecDeque;
+
+fn check_marker(mut marker: &VecDeque<char>) -> bool
+// fn check_marker(mut marker: &Vec<char>) -> bool
+{
+    println!("{:?}", marker);
+
+    let aux = marker;
+    for char in aux.iter().skip(1)
+    {
+        // println!("{},{}", marker[0], *char);
+        // if marker[0] == *char
+        // {
+            println!("------------------ Return falsers! ");
+            return false
+        // }
+    }
+    println!("Return truers! ----------------");
+    return true
+}
+
+fn e1(input: String) -> Option<VecDeque<char>>
+{
+    let mut input = input.chars();
+    let mut marker: VecDeque<char> = Default::default();
+    let window = 4;
+
+    for i in 0..window
+    {
+        marker.push_back(input.next().unwrap());
+    }
+    if check_marker(&mut marker)            // check if marker
+    {
+        println!("Marker:\t{:?}", marker);
+        return Some(marker)
+    }
+
+    for char in input
+    {
+        marker.pop_back();
+        marker.push_front(char);
+        if check_marker(&mut marker)        // check if marker
+        {
+            println!("Marker:\t{:?}", marker);
+            return Some(marker)
+        }
+    }
+    return None
+}
+
+fn main()
+{
+    let input = include_str!("../../input/06mini").to_string();     // Read input
+
+    e1(input);
+    // e2(input);
+}
---
+++ target/debug/deps/06-295f4bc55cdda63c.d
@@ -0,0 +1,6 @@
+/home/sergiza/CODE/AdventOfCode/aoc2022/target/debug/deps/06-295f4bc55cdda63c.rmeta: src/bin/06.rs src/bin/../../input/06mini
+
+/home/sergiza/CODE/AdventOfCode/aoc2022/target/debug/deps/06-295f4bc55cdda63c.d: src/bin/06.rs src/bin/../../input/06mini
+
+src/bin/06.rs:
+src/bin/../../input/06mini:
