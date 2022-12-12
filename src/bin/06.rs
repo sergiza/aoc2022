f37c2467f4f5b319ba212c539fe237ccdde1d0b6
“sergiza” <sergiza1994@gmail.com>
2022-12-12 16:57:48 +0100

    day6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -24,24 +24,30 @@ fn e1(input: String) -> Option<VecDeque<char>>
     let mut input = input.chars();
     let mut marker: VecDeque<char> = Default::default();
     let window = 4;
+    let mut c = window;
 
+    // first it
     for i in 0..window
     {
         marker.push_back(input.next().unwrap());
     }
+
     if check_marker(&mut marker)            // check if marker
     {
-        println!("Marker:\t{:?}", marker);
+        println!("Marker:\t{:?}\t -> {}", marker, c);
         return Some(marker)
     }
 
+    // rest it
     for char in input
     {
+        c = c + 1;
         marker.pop_back();
         marker.push_front(char);
+
         if check_marker(&mut marker)        // check if marker
         {
-            println!("Marker:\t{:?}", marker);
+            println!("Marker:\t{:?}\t -> {}", marker, c);
             return Some(marker)
         }
     }
